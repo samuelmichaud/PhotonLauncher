db188fff5c9627fb2e753b52ec3eb82c4df464cb
Samuel Michaud <samuel.michaud@gmail.com>
2022-07-31 22:00:22 +0200

    Launch app at startup (in production)

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,10 @@ app.on('activate', () => {
   }
 });
 
+app.setLoginItemSettings({
+  openAtLogin: isProductionEnv() // launch by default if the app is bundled for PROD
+});
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 ipcMain.on("exit-app", (event, args) => {
