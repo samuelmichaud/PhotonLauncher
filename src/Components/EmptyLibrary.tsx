8ccfb7597680987dbabe3d72b4fd2ab0da894bc9
Samuel M <samuel.michaud@gmail.com>
2022-10-11 21:36:28 +0200

    Change previous fix on unable to hover the menu when empty library

--- src/Components/EmptyLibrary.tsx
+++ src/Components/EmptyLibrary.tsx
@@ -12,11 +12,8 @@ interface EmptyLibraryWrapperProps {
 }
 
 const EmptyLibraryWrapper = styled.div<EmptyLibraryWrapperProps>`
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
+    position: relative;
+    height: 100%;
     color: white;
     display: flex;
     align-items: center;
